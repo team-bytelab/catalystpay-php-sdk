57ee02fe140ff4f2adc78e338b7235207c73929d
Yogesh Vakhre <yogesh.v@thebytelab.com>
2024-01-18 18:31:14 +0530

    Rename and make one test casesfor copyandpaycheckout

--- tests/CatalystPayTestCase/CopyAndPayCheckoutTest.php
+++ tests/CatalystPayTestCase/CopyAndPayCheckoutTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\CatalystPayIntegration;
+namespace Tests\CatalystPayTestCase;
 
 use Tests\TestCase;
 use CatalystPay\CatalystPaySDK;
@@ -12,25 +12,6 @@ class CopyAndPayCheckoutTest extends TestCase
      * Test the prepares a new checkout.
      */
     public function testPrepareCheckout()
-    {
-        $catalystPay = $this->getCatalystPayConfig();
-
-        // Checkout Values defined variable
-        $amount = 92.00;
-        $currency = 'EUR';
-        $paymentType = CatalystPaySDK::PAYMENT_TYPE_DEBIT;
-
-        $response = $catalystPay->prepareCheckout($amount, $currency, $paymentType);
-
-        // assert
-        $this->assertTrue($response->isCheckoutSuccess(), 'The checkout returned ' . $response->getResultCode() . ' instead of ' . CatalystPayResponseCode::CREATED_CHECKOUT);
-        $this->assertGreaterThanOrEqual(1, strlen($response->getId()));
-    }
-
-    /**
-     * Test the get the payment status for a checkout.
-     */
-    public function testGetPaymentStatus()
     {
         $catalystPay = $this->getCatalystPayConfig();
 
